ec7524f1a1b08b84ed15160b051507753970c4ae
EMCP <emcp@whichdegree.co>
2017-01-12 10:01:08 -0800

    Test is properly failing as expected with current code

--- test/app.e2e.js
+++ test/app.e2e.js
@@ -8,9 +8,8 @@ describe('app', () => {
   });
 
   it('should not be logged in', () => {
-    browser.waitForAngular();
     const logoutHeader = element(by.id('5-logout-header'));
-    console.log(logoutHeader);
-    expect(logoutHeader.isPresent() === undefined);
+    console.log(logoutHeader.getId());
+    expect(logoutHeader.isDisplayed()).toBe(false);
   });
 });
