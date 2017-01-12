a9efaf830e3d25be232f5f000672dfcc55e7c3fa
EMCP <emcp@whichdegree.co>
2017-01-12 09:55:05 -0800

    Attempting to make ids easier to find

--- test/app.e2e.js
+++ test/app.e2e.js
@@ -6,4 +6,11 @@ describe('app', () => {
   it('has a title', () => {
     expect(browser.getTitle()).toEqual('Mahr.io - Learning Management System');
   });
+
+  it('should not be logged in', () => {
+    browser.waitForAngular();
+    const logoutHeader = element(by.id('5-logout-header'));
+    console.log(logoutHeader);
+    expect(logoutHeader.isPresent() === undefined);
+  });
 });
